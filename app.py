04f85895d9847dfae790b47b73909307ae0ee9bb
shovroman19122008-png <shovroman19122008@gmail.com>
2026-05-14 08:05:43 +0500

    обавил railway.json и nixpacks.toml

--- app.py
+++ app.py
@@ -608,4 +608,5 @@ def create_review(order_id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
